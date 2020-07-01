fa86be21e776906a6f5f77d17f320acfe15a0e64
AnaBrando <anapchavesweb@gmail.com>
2020-07-01 17:22:53 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,14 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'navegacao',
+    loadChildren: () => import('./navegacao/navegacao.module').then( m => m.NavegacaoPageModule)
+  },
+  {
+    path: 'botoes',
+    loadChildren: () => import('./botoes/botoes.module').then( m => m.BotoesPageModule)
   }
 ];
 
